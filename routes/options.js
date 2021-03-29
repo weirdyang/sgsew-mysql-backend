8f4cf5da549cb2354df06d50db0e7bb70de21dcc
weirdyang <wy4786@gmail.com>
2021-03-29 14:59:57 +0800

    added error handling

--- routes/options.js
+++ routes/options.js
@@ -18,6 +18,9 @@ router.get('/', async (req, res, next) => {
 router.get('/company/:companyId', async (req, res, next) => {
   try {
     const results = await getOptionsByCompanyId(req.params.companyId);
+    if (!results.length) {
+      return next(new HttpError('no options linked to company id', 404));
+    }
     return res.json(results);
   } catch (error) {
     return next(new HttpError(error.message, 500));
@@ -27,6 +30,9 @@ router.get('/company/:companyId', async (req, res, next) => {
 router.get('/:optionId', async (req, res, next) => {
   try {
     const results = await getOptionsById(req.params.optionId);
+    if (!results.length) {
+      return next(new HttpError('no such option', 404));
+    }
     return res.json(results);
   } catch (error) {
     return next(new HttpError(error.message, 500));
